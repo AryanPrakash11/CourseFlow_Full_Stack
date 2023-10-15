8250d24215906a6dc256074b379b2fa903be5f87
AryanPrakash11 <prakasharyan227@gmail.com>
2023-10-15 13:44:50 +0530

    zptoject upload

---
+++ frontend/src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import { Box, Heading, HStack, Stack, VStack } from '@chakra-ui/react';
+import React from 'react';
+import {
+  TiSocialYoutubeCircular,
+  TiSocialInstagramCircular,
+} from 'react-icons/ti';
+import { DiGithub } from 'react-icons/di';
+const Footer = () => {
+  return (
+    <Box padding={'4'} bg="blackAlpha.900" minH={'10vh'}>
+      <Stack direction={['column', 'row']}>
+        <VStack alignItems={['center', 'flex-start']} width="full">
+          <Heading children="All Rights Reserved" color={'white'} />
+          <Heading
+            fontFamily={'body'}
+            size="sm"
+            children="@CourseFlow"
+            color={'yellow.400'}
+          />
+        </VStack>
+
+        <HStack
+          spacing={['2', '10']}
+          justifyContent="center"
+          color={'white'}
+          fontSize="50"
+        >
+          <a href="https://www.youtube.com/@paveit1475" target={'blank'}>
+            <TiSocialYoutubeCircular />
+          </a>
+          <a href="https://www.instagram.com/_aryan_prakash_1/" target={'blank'}>
+            <TiSocialInstagramCircular />
+          </a>
+          <a href="https://github.com/AryanPrakash11" target={'blank'}>
+            <DiGithub />
+          </a>
+        </HStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
